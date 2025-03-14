340b1846f3793074481f4a4bec8b8e8644abee14
Jacob <jacobseated@gmail.com>
2025-03-14 02:02:57 +0100

    script to prepare deployment

---
+++ deploy-prepare.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+OVERLEAF_DIR=./overleaf
+OVERRIDE_FILE=./docker-compose.override.yml
+REPO_URL="https://github.com/overleaf/overleaf.git"
+
+if [ -d "$OVERLEAF_DIR" ]; then
+    echo "Removing old Overleaf installation..."
+    rm -rf "$OVERLEAF_DIR"
+fi
+
+echo "Cloning Overleaf repository..."
+git clone "$REPO_URL" "$OVERLEAF_DIR"
+
+cp "$OVERRIDE_FILE" "$OVERLEAF_DIR/"
+
+echo "Copying Overleaf service file to /etc/systemd/system/..."
+sudo cp "./overleaf.service" "/etc/systemd/system/"
+
+echo "Copying Overleaf service file to /etc/systemd/system/..."
+sudo cp "./overleaf.service" "/etc/systemd/system/"
+
+echo "Reloading systemd and enabling service..."
+sudo systemctl daemon-reload
+sudo systemctl enable overleaf.service
+# sudo systemctl start overleaf.service
